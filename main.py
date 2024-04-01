e82f5c23af3ef172634e31af8835e25873135282
Nikhil <nikhilkasaghatta@gmail.com>
2024-04-01 19:46:23 -0400

    testing commit + push

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print('testing github repo')
 
 
 # Press the green button in the gutter to run the script.
